c37ed46b783c7a0d37e4f9a2461b6aff23de80cc
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-08-31 19:37:08 +0600

    AnimatedIcon Widget

--- lib/my_home.dart
+++ lib/my_home.dart
@@ -7,52 +7,50 @@ class MyHome extends StatefulWidget {
   State<MyHome> createState() => _MyHomeState();
 }
 
-class _MyHomeState extends State<MyHome> {
-  String userPost = '';
+class _MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
 
-  final _textController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  bool videoPlaying = false;
+
+  void _iconTapped() {
+    if (videoPlaying == false) {
+      _animationController.forward();
+      videoPlaying = true;
+    } else {
+      _animationController.reverse();
+      videoPlaying = false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(44.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(
-                  userPost,
-                  style: const TextStyle(fontSize: 32),
-                ),
-              ),
-            ),
-            TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                hintText: 'What\'s on your mind?',
-                border: const OutlineInputBorder(),
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    _textController.clear();
-                  },
-                  icon: const Icon(Icons.clear),
-                ),
-              ),
-            ),
-            MaterialButton(
-              color: Colors.blue,
-              onPressed: () {
-                userPost = _textController.text;
-                setState(() {});
-              },
-              child: const Text('Post'),
-            ),
-          ],
+      backgroundColor: Colors.deepPurple[100],
+      body: Center(
+        child: GestureDetector(
+          onTap: _iconTapped,
+          child: AnimatedIcon(
+            icon: AnimatedIcons.menu_home,
+            progress: _animationController,
+            size: 156,
+          ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController;
   }
 }
